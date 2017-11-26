0e83332f12697b024641b4298c0deda349c53aa2
meghanjwyatt <33995250+meghanjwyatt@users.noreply.github.com>
2017-11-26 07:07:28 -0800

    Framework for data input

---
+++ datainput.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import numpy
+
+class DataInput(object):
+    '''
+    Framework for loading user-specified data. Enables input of data via files
+    or via ``manual`` user input. 
+    '''
+    def __init__(self):
+        pass
+        
+    def run
