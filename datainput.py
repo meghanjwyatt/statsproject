9dc5ec5552e934004011272c22df55cd3eff6ead
meghanjwyatt <33995250+meghanjwyatt@users.noreply.github.com>
2017-11-26 07:28:40 -0800

    Updated docstring with requirements

--- datainput.py
+++ datainput.py
@@ -5,8 +5,31 @@ class DataInput(object):
     '''
     Framework for loading user-specified data. Enables input of data via files
     or via ``manual`` user input. 
+    
+    Types of data that may need to be loaded:
+    
+    One data set of arbitrary size:
+        2) mean confidence limits
+        4) confidence limits for a sample using simulation
+        5a) Large and small sample tests for population mean
+        5
+        
+    Two data sets of arbitrary sizes:
+        3) Confidence limits for the difference between samples
+        5b) Large and small smaple tests for the difference between two means
+    
+    Two data sets of same size:
+        1) Q-Q plot for a set of x/y data (???)
+        5c) Chi-squared test
+        6a) Correlation coefficient of x/y data
+        6b) Carry out hypothesis testing on the correlation coefficient (H0 = (r = 0))
+        6d) Perform linear regression on a set of x/y data
+        
+    Also, we need data input for propagation of error calculations
+        
     '''
     def __init__(self):
         pass
         
-    def run
+    def load_from_file(self, filepath):
+        data = numpy.loadtxt(filepath)
