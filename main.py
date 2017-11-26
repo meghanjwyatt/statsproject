6f442916f752b7d0f52badbb23a3f33399373a4a
meghanjwyatt <33995250+meghanjwyatt@users.noreply.github.com>
2017-11-25 21:40:19 -0800

    Use interactive input instead of command line arguments

--- main.py
+++ main.py
@@ -4,18 +4,32 @@ import numpy
 
 class StatsTool(object):
     def __init__(self):
-        self._parse_args()
+        msg = \
+'''
+-----------------------------
+statstool.py
+
+By Meghan Wyatt and David Lee
+-----------------------------
+
+Analysis options:
+1) Plot a Q-Q plot for a set of x/y data
+2) Compute mean confidence limits
+3) Compute confidences limits for the difference between samples
+4) Compute mean confidence limits for a sample via bootstrapping
+5a) Tests for population mean
+5b) Tests for difference between means of two populations
+5c) Chi-Squared test
+6a) Compute correlation coefficient ("r") for a set of x/y data
+6b) Carry out hypothesis testing on the correlation coefficient
+6d) Perform a linear regression on a set of x/y data
+7) Propagation of error
+-----------------------------
+
+'''
+        print(msg)
+        analysistype = input("Please input the number (or number/letter combination) "
+                             "of the desired analysis.")
         
-    def _parse_args(self):
-        parser = argparse.ArgumentParser()
         
-        
-        parser.add_argument('--load-from-file', required=False, dest='datafile",
-                            default=None,
-                            help="Load data from the specified file. Data should be "
-                                 "formatted in columns (ASCII format). The number of "
-                                 "columns should match the required number of datasets "
-                                 "for the specified analysis.  See analysis options "
-                                 "for details."
-                           )
-        parser.add_argument('--
+
