8e90e9edff487639f6493f033229a7823d964cff
Meghan Wyatt <meghanjwyatt@gmail.com>
2017-12-02 08:04:34 -0500

    Updated doc string; started writing analysis

--- analysis1.py
+++ analysis1.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 '''
-Take in two samples of the same size and generate a Q-Q plot.
+Generate a quantile-quantile plot, given an array of data.
 '''
 
-def run(dataset1, dataset2):
+def run(dataset):
+    '''
+    -----------
+    Parameters:
+    -----------
+    dataset: (numpy.ndarray) a 1-dimensional numpy array of data.
+
+    -----------------
+    Function
+    -----------------
+    Displays Q-Q plot, comparing normalized data to N(0,1) distribution
+     
+    '''
+    # sample mean
+    mean = dataset.mean()
+    # square root of sample variance
+    stdev = numpy.sqrt(dataset.var(ddof=1))
+
+    normalized_dataset = (dataset - mean)/stdev
+    normalized_dataset.sort()
+    
     pass
