56720922f284fa2837d8c13cdde9f5be96a26183
Meghan Wyatt <meghanjwyatt@gmail.com>
2017-12-02 09:14:38 -0500

    Set up docstring for analysis 3

---
+++ analysis3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import numpy
+
+def run(dataset1, dataset2, alpha=0.05):
+    '''
+    -----------
+    Parameters:
+    -----------
+    dataset1: (numpy.ndarray) a 1-dimensional numpy array of data; arbitrary 
+        length
+    dataset2: (numpy.ndarray) a 1-dimensional numpy array of data; arbitrary
+        length
+
+    -----------------
+    Function
+    -----------------
+    Prints to stdout the confidence limits for the difference between the
+    samples dataset1 and dataset2, at the specified alpha level.
+    '''
+     
