58a03de8a1f413365729187f7e6e7c0ae11dccb1
Meghan Wyatt <meghanjwyatt@gmail.com>
2017-12-02 11:33:49 -0500

    Moved analysis-specific input functions to separate file

--- io/data.py
+++ io/data.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import numpy
-import query
 
 '''
 Types of data that may need to be loaded:
@@ -25,116 +24,6 @@ Also, we need data input for propagation of error calculations (7), and we need 
 allow input of single values, for use with (5a) large and small sample tests for 
 population mean.
 '''
-
-def input1():
-    '''
-    Get input for a Q-Q plot of x/y data
-    '''
-    msg1 = "Please enter the first sample."
-    dataset1 = UserDataSet(msg1).data
-    
-    msg2 = ("Please enter the second sample, which should be the same length "
-           "as the first sample.")
-    dataset2 = UserDataSet(msg2, requiredsize=dataset1.shape[0]).data
-    
-    return dataset1, dataset2
-
-def input2():
-    '''
-    Get input computing mean confidence limits.
-    '''
-    msg = "Please enter the values of the sample."
-    dataset = UserDataSet(msg).data
-    return dataset
-
-def input3():
-    '''
-    Get input for computing confidence limits for the difference between two samples
-    '''
-    msg1 = "Please enter the first sample."
-    dataset1 = UserDataSet(msg1).data
-    
-    msg2 = "Please enter the second sample."
-    dataset2 = UserDataSet(msg2).data
-    return dataset1, dataset2
-
-def input4():
-    '''
-    Get input for computing confidence limits of a sample using simulation
-    '''
-    msg = "Please enter the values of the sample."
-    dataset = UserDataSet(msg).data
-    return dataset
-
-def input5a():
-    '''
-    Get input for testing the hypothesis that the mean of a specified sample is 
-    equal to a specified value.
-    '''
-    msg = "Please enter the values of the sample."
-    dataset = UserDataSet(msg).data
-    proposedmean = float(input("Please enter the proposed mean and press then "
-                               "press the enter key\n"))
-    return dataset, proposedmean
-
-def input5b():
-    '''
-    Get input for testing whether there is a difference between two means.
-    '''
-    msg1 = "Please enter the values of the first sample."
-    dataset1 = UserDataSet(msg1).data
-    msg2 = "Please enter the values of the second sample."
-    dataset2 = UserDataSet(msg2).data
-    return dataset1, dataset2
-
-def input5c():
-    '''
-    Get input for a chi-squared test.
-    '''
-    datasets = []
-    msg1 = "Input the number of values in each category, for the first group."
-    datasets.append(UserDataSet(msg1).data)
-    msg2 = "Input the number of values in each category, for the second group."
-    datasets.append(UserDataSet(msg2).data)
-
-    dataset_idx = 3
-    add_dataset = query.binary_query("Enter another sample?")
-    while add_dataset:
-        msg = "Input the number of values in ech category, for group #{:d}"\
-              .format(dataset_idx)
-        datasets.append(UserDataSet(msg).data)
-        dataset_idx += 1
-
-    return datasets
-
-def input6():
-    '''
-    Get input for calculating correlation coefficient of x/y data, carrying 
-    out hypothesis testing on the correlation coefficient, or performing a linear
-    regression on the x/y data.
-    
-    Return the following:
-        x_values: numpy array of the x values
-        y_values: numpy array of the y values, where the i^th y value
-            corresponds to the i^th x value.
-        y_errors: if specified, a numpy array of error bars (e.g., standard
-            deviations) for the y values. If not specified, return None.
-    '''
-    msg1 = "Input the x values."
-    x_values = UserDataSet(msg1).data
-    msg2 = ("Input the y values. The number of y values should be the same as the "
-           "number of x values, and the i^th y value should correspond to the "
-           "i^th x value.")
-    y_values = UserDataSet(msg2, requiredsize = x_values.shape[0]).data
-    # Query the user regarding the inclusion of error bars
-    includeerrorbars = query.binary_query("Would you like to include error bars?")
-    if includeerrorbars:
-        msg3 = "Input the error bars associated with y values."
-        y_errors = UserDataSet(msg3, requiredsize = x_values.shape[0]).data
-    else:
-        y_errors = None
-    return x_values, y_values, y_errors
-
 
 class UserDataSet(object):
     '''
