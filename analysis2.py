321ab425b3c8d61fa6953eb602ee216c32d38080
Meghan Wyatt <meghanjwyatt@gmail.com>
2017-12-02 08:15:26 -0500

    Updated docstring

--- analysis2.py
+++ analysis2.py
@@ -3,9 +3,16 @@ import numpy
 
 def run(dataset1, dataset2, alpha=0.05):
     '''
-    Compute the confidence limits for the difference between the samples
-    dataset1 and dataset2, at the specified alpha-level.  Print the results.
-    
-    dataset1 and dataset2 are both numpy arrays of arbitrary size.
+    -----------
+    Parameters:
+    -----------
+    dataset1: (numpy.ndarray) a 1-dimensional numpy array of data.
+    dataset2: (numpy.ndarray) a 1-dimensional numpy array of data.
+
+    -----------------
+    Function
+    -----------------
+    Prints to stdout the confidence limits for the difference between the
+    samples dataset1 and dataset2, at the specified alpha level.
     '''
     
