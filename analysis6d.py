9f698ce30fc450a3ee3682e8fa9af13ee69fa9cb
Meghan Wyatt <meghanjwyatt@gmail.com>
2017-12-02 10:39:17 -0500

    Updated docstring

--- analysis6d.py
+++ analysis6d.py
@@ -3,10 +3,9 @@ import numpy
 import scipy.stats
 
 '''
-Compute the probability that an uncorrelated distribution of x/y values produces
-the correlation at least as extreme as that observed with the data.
+Perform a linear regression.
 '''
-def run(x_values, y_values, y_errors, alpha=0.05):
+def run(x_values, y_values, y_errors):
     '''
     -----------
     Parameters:
@@ -15,11 +14,10 @@ def run(x_values, y_values, y_errors, alpha=0.05):
     y_values: (numpy.ndarray) a 1-dimensional numpy array of y-values, where
         for each i=1,2,...,n x_values[i] is paired with y_values[i].
     y_errors: (numpy.ndarray or None) the errors associated with each value
-    alpha: (float) significance level for hypothesis testing
 
     -----------------
     Function
     -----------------
-    Compute the pearson correlation coefficient and print to stdout.
+    Perform a linear regression and show the resulting plot.
     '''
     
