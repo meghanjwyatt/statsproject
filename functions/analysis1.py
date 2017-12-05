c255fc01b07c1299588badd5a194f6c5b6b5f9ac
meghanjwyatt <33995250+meghanjwyatt@users.noreply.github.com>
2017-12-05 09:20:43 -0800

    simplified method

--- functions/analysis1.py
+++ functions/analysis1.py
@@ -20,29 +20,8 @@ def run(dataset):
     Displays Q-Q plot, comparing normalized data to N(0,1) distribution
      
     '''
-    # sample mean
-    mean = dataset.mean()
-    # square root of sample variance
-    stdev = numpy.sqrt(dataset.var(ddof=1))
-
-    normalized_dataset = (dataset - mean)/stdev
-    normalized_dataset.sort()
-
-    n = normalized_dataset.shape[0]
-    quantiles = (numpy.arange(1, n+1, dtype=float)-0.5)/n
-    
-    theoretical_values = scipy.stats.norm.ppf(quantiles)
-
-    pyplot.plot(normalized_dataset, theoretical_values, 'o', color='black')
-
-    pyplot.xlabel("Input data (normalized)")
-    pyplot.ylabel("Normal distribution")
-
-    xmin = min(normalized_values.min(), theoretical_values.min())
-    xmax = max(normalized_values.max(), theoretical_values.max())
-
-    # Plot a diagonal line
-    pyplot.plot((xmin, xmax) (xmin, xmax), color='black')
-
+    #Calculate quantile values
+    scipy.stats.probplot(dataset, plot=pyplot)
+    #Display plot
     pyplot.show()
     return
