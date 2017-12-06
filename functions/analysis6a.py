f17a0fbf2f2d16fe667eb0ae037616d5c18aa21c
meghanjwyatt <33995250+meghanjwyatt@users.noreply.github.com>
2017-12-05 20:16:54 -0800

    Trying weighted correlation analysis

--- functions/analysis6a.py
+++ functions/analysis6a.py
@@ -6,6 +6,12 @@ import scipy.stats
 Compute the correlation coefficient for the x-values in x_values and y-values
 in y_values.
 '''
+def covariance(x, y, w):
+    numpy.sum(w*(x - numpy.average(x, weights=w))*(y - numpy.average(y, weights=w))/w.sum()
+              
+def correlation(x, y, w):
+    covariance(x,y,w)/numpy.sqrt(covariance(x,x,w)*covariance(y,y,w))
+              
 def run(x_values, y_values, y_errors):
     '''
     -----------
@@ -14,7 +20,7 @@ def run(x_values, y_values, y_errors):
     x_values: (numpy.ndarray) a 1-dimensional numpy array of x-values
     y_values: (numpy.ndarray) a 1-dimensional numpy array of y-values, where
         for each i=1,2,...,n x_values[i] is paired with y_values[i].
-    y_errors: (numpy.ndarray or None) the errors associated with each value
+    y_errors: (numpy.ndarray or None) the uncertainties (standard deviation) associated with each value
 
     -----------------
     Function
@@ -24,7 +30,7 @@ def run(x_values, y_values, y_errors):
     if y_errors is None:
         r, p = scipy.stats.pearsonr(x_values, y_values)
     else:
-        raise NotImplementedError # need to do this by hand
+        r = correlation(x_values, y_values, y_errors)
     msg = '''
 --------------------------------------------------------------------------------
 Calculation of correlation coefficient for specified x-values and specified 
