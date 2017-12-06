47ec026a5081af2aed6d164350f76e320c7650f5
meghanjwyatt <33995250+meghanjwyatt@users.noreply.github.com>
2017-12-05 20:19:47 -0800

    added print statement

--- functions/analysis6a.py
+++ functions/analysis6a.py
@@ -7,10 +7,10 @@ Compute the correlation coefficient for the x-values in x_values and y-values
 in y_values.
 '''
 def covariance(x, y, w):
-    numpy.sum(w*(x - numpy.average(x, weights=w))*(y - numpy.average(y, weights=w))/w.sum()
+    return numpy.sum(w*(x - numpy.average(x, weights=w))*(y - numpy.average(y, weights=w)))/w.sum()
               
 def correlation(x, y, w):
-    covariance(x,y,w)/numpy.sqrt(covariance(x,x,w)*covariance(y,y,w))
+    return covariance(x,y,w)/numpy.sqrt(covariance(x,x,w)*covariance(y,y,w))
               
 def run(x_values, y_values, y_errors):
     '''
@@ -21,7 +21,6 @@ def run(x_values, y_values, y_errors):
     y_values: (numpy.ndarray) a 1-dimensional numpy array of y-values, where
         for each i=1,2,...,n x_values[i] is paired with y_values[i].
     y_errors: (numpy.ndarray or None) the uncertainties (standard deviation) associated with each value
-
     -----------------
     Function
     -----------------
@@ -37,4 +36,5 @@ Calculation of correlation coefficient for specified x-values and specified
 y-values
 --------------------------------------------------------------------------------
 pearson correlation coefficient (r): {:.04e}'''.format(r)
+    print(msg)
     return
